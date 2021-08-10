3e82b6908507ff95f4ecd5712b230476f72f36ed
ilya <tsekh11@gmail.com>
2021-08-10 19:14:07 +0300

    + Login Form with validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import s from './Login.module.css'
 
-const Login = () => {
-    return <h1>LOGIN Please</h1>
-}
+const LoginForm = () => {
+    return (
+        <Formik
+            initialValues={{ login: '', password: '', checked: [] }}
+            validationSchema={Yup.object({
+                login: Yup.string()
+                    .min(4, 'Must be 4 characters or more')
+                    .max(15, 'Must be 15 characters or less')
+                    .required('Required'),
+                password: Yup.string()
+                    .min(4, 'Must be 4 characters or more')
+                    .max(20, 'Must be 20 characters or less')
+                    .required('Required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            <Form className={s.form_wrapper} >
+                <label htmlFor="login" className={s.label} >Login</label>
+                <Field name="login" type="text" className={s.inputStyle} />
+                <span className={s.errorStyle}>
+                    <ErrorMessage name="login" />
+                </span>
+
+                <label htmlFor="password" className={s.label} >Password</label>
+                <Field name="password" type="text" className={s.inputStyle} />
+                <span className={s.errorStyle}>
+                     <ErrorMessage name="password" />
+                </span>
+
+                <label className={s.label} >
+                <Field type="checkbox" name="checked" value="Yes" />
+                remember me
+                </label>
+
+                <button type="submit" className={s.button}>Submit</button>
+            </Form>
+        </Formik>
+    );
+};
+
+const Login = () => <div className={s.wrapper}>
+    <h3>Login please</h3>
+    <LoginForm />
+</div>
 
 export default Login
