daa4bbcefc669c84b0b1de3b2970772c4a6112ef
ilya <tsekh11@gmail.com>
2021-08-26 01:21:36 +0300

    Updates

---
+++ precache-manifest.ae04045d118655a2ba6f16bd8170985f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930e2821145b621af32bb01aa34d2b3b",
+    "url": "/tsekh11/index.html"
+  },
+  {
+    "revision": "e178cc305d51d775c67b",
+    "url": "/tsekh11/static/css/3.2ffa771e.chunk.css"
+  },
+  {
+    "revision": "1bead2ee77037754b42a",
+    "url": "/tsekh11/static/css/4.fcedad56.chunk.css"
+  },
+  {
+    "revision": "432a317feb946f725650",
+    "url": "/tsekh11/static/css/main.62d270f0.chunk.css"
+  },
+  {
+    "revision": "84b489405c26365814f2",
+    "url": "/tsekh11/static/js/2.e956d38d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tsekh11/static/js/2.e956d38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e178cc305d51d775c67b",
+    "url": "/tsekh11/static/js/3.6895f348.chunk.js"
+  },
+  {
+    "revision": "1bead2ee77037754b42a",
+    "url": "/tsekh11/static/js/4.ce327145.chunk.js"
+  },
+  {
+    "revision": "432a317feb946f725650",
+    "url": "/tsekh11/static/js/main.5ad430f0.chunk.js"
+  },
+  {
+    "revision": "2603e77a4c6ee685444d",
+    "url": "/tsekh11/static/js/runtime-main.4c1aaa32.js"
+  },
+  {
+    "revision": "52a9e87e539bfec58634f5a5072a3f46",
+    "url": "/tsekh11/static/media/logo.52a9e87e.png"
+  },
+  {
+    "revision": "5f4fcfce4e6484276e1c462456ef505a",
+    "url": "/tsekh11/static/media/userlogo.5f4fcfce.png"
+  }
+]);
