077a28ed744b71000ffa35ad703a4529c44ec6bd
ilya <tsekh11@gmail.com>
2021-09-19 23:59:55 +0300

    + add typescript,auth-reducer types

--- src/Redux/auth-reducer.tsx
+++ src/Redux/auth-reducer.tsx
@@ -4,18 +4,21 @@ const SET_LOGIN = 'auth/SET-LOGIN';
 const LOGIN_ERROR = 'auth/LOGIN-ERROR';
 const GET_CAPTCHA = 'auth/GET-CAPTCHA';
 
+
 const initialState = {
-    id: null,
-    login: null,
-    email: null,
-    isAuth: null,
+    id: null as null | number,
+    login: null as null | string,
+    email: null as null | string,
+    isAuth: null as null | boolean,
     errorMessage: '',
     isErrorLogin: false,
     isInitialized: false,
-    captchaUrl: null
+    captchaUrl: null as null | string
 }
 
-const authReducer = (state = initialState, action) => {
+export type InitialStateType = typeof initialState
+
+const authReducer = (state = initialState, action: any): InitialStateType => {
     switch (action.type) {
         case SET_LOGIN:
             return {
@@ -38,18 +41,19 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
-export const setLogin = (id, login, email, isAuth) => ({type: SET_LOGIN, data: {id, login, email, isAuth}});
-export const setLoginError = (message) => ({type: LOGIN_ERROR, message});
-export const setCaptchaUrl = (data) => ({type: GET_CAPTCHA, data});
+export const setLogin: (id: number | null, login: string | null, email: string | null, isAuth: boolean | null) => ({type: typeof SET_LOGIN, data: object})
+                    = (id, login, email, isAuth) => ({type: SET_LOGIN, data: {id, login, email, isAuth}});
+export const setLoginError: (message: string) => ({type: typeof LOGIN_ERROR, message: string}) = (message) => ({type: LOGIN_ERROR, message});
+export const setCaptchaUrl: (data: string) => ({type: typeof GET_CAPTCHA, data: string}) = (data) => ({type: GET_CAPTCHA, data});
 
-export const getAuth = () => async (dispatch) => {
+export const getAuth = () => async (dispatch: any) => {
     let response = await authAPI.getAuthData();
     if (response.data.resultCode === 0) {
         let {id, login, email} = response.data.data;
         dispatch(setLogin(id, login, email, true))
     }
 }
-export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+export const login = (email: string, password: string, rememberMe: boolean, captcha: boolean) => async (dispatch: any) => {
     let response = await authAPI.loginAuth(email, password, rememberMe, captcha);
     if (response.data.resultCode === 0) {
         dispatch(getAuth())
@@ -60,13 +64,13 @@ export const login = (email, password, rememberMe, captcha) => async (dispatch) 
         dispatch(setLoginError(response.data.messages[0]))
     }
 }
-export const logout = () => async (dispatch) => {
+export const logout = () => async (dispatch: any) => {
     let response = await authAPI.logoutAuth();
     if (response.data.resultCode === 0) {
         dispatch(setLogin(null, null, null, false))
     }
 }
-export const getCaptcha = () => async (dispatch) => {
+export const getCaptcha = () => async (dispatch: any) => {
     let response = await securityAPI.getCaptchaUrl();
     dispatch(setCaptchaUrl(response.data.url))
 
