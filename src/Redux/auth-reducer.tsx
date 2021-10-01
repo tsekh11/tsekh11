9e02ad437b69e5e14d104272a1bf6b79d7c60908
ilya <tsekh11@gmail.com>
2021-10-01 15:38:25 +0300

    + action and thunk typing in auth-reducer

--- src/Redux/auth-reducer.tsx
+++ src/Redux/auth-reducer.tsx
@@ -1,4 +1,5 @@
 import {authAPI, securityAPI} from "../api/DAL";
+import {ThunkAction} from "redux-thunk";
 
 const SET_LOGIN = 'auth/SET-LOGIN';
 const LOGIN_ERROR = 'auth/LOGIN-ERROR';
@@ -18,7 +19,7 @@ const initialState = {
 
 export type InitialStateType = typeof initialState
 
-const authReducer = (state = initialState, action: any): InitialStateType => {
+const authReducer = (state = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case SET_LOGIN:
             return {
@@ -41,19 +42,25 @@ const authReducer = (state = initialState, action: any): InitialStateType => {
     }
 }
 
-export const setLogin: (id: number | null, login: string | null, email: string | null, isAuth: boolean | null) => ({type: typeof SET_LOGIN, data: object})
-                    = (id, login, email, isAuth) => ({type: SET_LOGIN, data: {id, login, email, isAuth}});
-export const setLoginError: (message: string) => ({type: typeof LOGIN_ERROR, message: string}) = (message) => ({type: LOGIN_ERROR, message});
-export const setCaptchaUrl: (data: string) => ({type: typeof GET_CAPTCHA, data: string}) = (data) => ({type: GET_CAPTCHA, data});
+type SetLoginType = {type: typeof SET_LOGIN, data: object}
+type SetLoginErrorType = {type: typeof LOGIN_ERROR, message: string}
+type SetCaptchaUrlType = {type: typeof GET_CAPTCHA, data: string}
 
-export const getAuth = () => async (dispatch: any) => {
+type ActionsType = SetCaptchaUrlType | SetLoginErrorType | SetLoginType
+type ThunkType = ThunkAction<Promise<void>, any, any, ActionsType>
+
+export const setLogin = (id: number | null, login: string | null, email: string | null, isAuth: boolean): SetLoginType => ({type: SET_LOGIN, data: {id, login, email, isAuth}});
+export const setLoginError = (message: string): SetLoginErrorType => ({type: LOGIN_ERROR, message});
+export const setCaptchaUrl = (data: string): SetCaptchaUrlType => ({type: GET_CAPTCHA, data});
+
+export const getAuth = (): ThunkType => async (dispatch) => {
     let response = await authAPI.getAuthData();
     if (response.data.resultCode === 0) {
         let {id, login, email} = response.data.data;
         dispatch(setLogin(id, login, email, true))
     }
 }
-export const login = (email: string, password: string, rememberMe: boolean, captcha: boolean) => async (dispatch: any) => {
+export const login = (email: string, password: string, rememberMe: boolean, captcha: boolean): ThunkType => async (dispatch) => {
     let response = await authAPI.loginAuth(email, password, rememberMe, captcha);
     if (response.data.resultCode === 0) {
         dispatch(getAuth())
@@ -64,16 +71,15 @@ export const login = (email: string, password: string, rememberMe: boolean, capt
         dispatch(setLoginError(response.data.messages[0]))
     }
 }
-export const logout = () => async (dispatch: any) => {
+export const logout = (): ThunkType => async (dispatch) => {
     let response = await authAPI.logoutAuth();
     if (response.data.resultCode === 0) {
         dispatch(setLogin(null, null, null, false))
     }
 }
-export const getCaptcha = () => async (dispatch: any) => {
+export const getCaptcha = (): ThunkType => async (dispatch) => {
     let response = await securityAPI.getCaptchaUrl();
     dispatch(setCaptchaUrl(response.data.url))
-
 }
 
 export default authReducer;
